aff648085f4d0dfaaf05220b35eeec4e07d976de
Shtaiven <etienneshorty@gmail.com>
2014-11-15 01:29:09 -0500

    Debugging Database Getters

--- LehmanHackathon2014/src/Database.java
+++ LehmanHackathon2014/src/Database.java
@@ -8,7 +8,7 @@ import java.sql.Statement;
 
 public class Database {
 	
-	public static ArrayList<String> getEvents(ArrayList<String> tags) {
+	public static ArrayList<String> findEvents(ArrayList<String> tags) {
 		
 		/**
 		 * returns an ArrayList of event names from the database which contain the tags specified in the 
@@ -48,16 +48,145 @@ public class Database {
 	}
 	
 	
-	private static Connection getConnection() {
-		Connection connection = null;
+	public static String getLocation(String event) {
+		
 		try {
+			
+			Connection connection = getConnection();
+			String selectStatement = "select LOCATION from EVENTS where EVENTNAME='"+ event +"'";
+			Statement sqlStatement = connection.createStatement();
+			ResultSet resultSet = sqlStatement.executeQuery(selectStatement);
+			String location = null;
+			
+			while (resultSet.next()) {
+				
+				location = resultSet.getString(2);
+				
+			}
+			
+			return location;
+			
+		} catch (SQLException e) {
+			
+			System.out.println(e.getStackTrace());
+			
+		}	
+		
+		return null;	
+		
+	}
+	
+	public static String getTime(String event) {
+		
+		try {
+			
+			Connection connection = getConnection();
+			String selectStatement = "select EVENTTIME from EVENTS where EVENTNAME='"+ event +"'";
+			Statement sqlStatement = connection.createStatement();
+			ResultSet resultSet = sqlStatement.executeQuery(selectStatement);
+			
+			return resultSet.getString(3);
+			
+		} catch (SQLException e) {
+			
+			System.out.println(e.getStackTrace());
+			
+		}	
+		
+		return null;	
+		
+	}
+	
+	public static ArrayList<String> getTags(String event) {
+		
+		try {
+			
+			Connection connection = getConnection();
+			ArrayList<String> tags = new ArrayList<String>(3);
+			
+			for (Integer i = 1; i <=3; i++) {
+				
+				String selectStatement = "select EVENTNAME from EVENTS where TAG" + i.toString() +"=";
+				Statement sqlStatement = connection.createStatement();
+				ResultSet resultSet = sqlStatement.executeQuery(selectStatement);
+				tags.add(resultSet.getString(3+i));
+				
+			}
+			
+			return tags;
+			
+		} catch (SQLException e) {
+			
+			System.out.println(e.getStackTrace());
+			
+		}	
+		
+		return null;
+		
+	}
+	
+	public static String getCost(String event) {
+		
+		try {
+			
+			Connection connection = getConnection();
+			String selectStatement = "select COST from EVENTS where EVENTNAME='"+ event +"'";
+			Statement sqlStatement = connection.createStatement();
+			ResultSet resultSet = sqlStatement.executeQuery(selectStatement);
+			
+			Double cost = resultSet.getDouble(7);
+			String costString = "$" + cost.toString();
+			
+			return costString;
+			
+		} catch (SQLException e) {
+			
+			System.out.println(e.getStackTrace());
+			
+		}	
+		
+		return null;
+		
+	}
+	
+	public static String getInfo(String event) {
+		
+		try {
+			
+			Connection connection = getConnection();
+			String selectStatement = "select INFO from EVENTS where EVENTNAME='"+ event +"'";
+			Statement sqlStatement = connection.createStatement();
+			ResultSet resultSet = sqlStatement.executeQuery(selectStatement);
+			
+			return resultSet.getString(8);
+			
+		} catch (SQLException e) {
+			
+			System.out.println(e.getStackTrace());
+			
+		}	
+		
+		return null;
+		
+	}
+	
+	private static Connection getConnection() {
+		
+		Connection connection = null;
+		
+		try {
+			
 			String connectionString = "jdbc:hsqldb:hsql://localhost:9001";
 			connection = DriverManager.getConnection(connectionString);
-		}
-		catch (SQLException e) {
+			
+		} catch (SQLException e) {
+			
 			System.out.println(e.getStackTrace());
+			
 		}
+		
 		return connection;
+		
 	}
 	
 	private static Boolean searchList(ArrayList<String> list, String item) {
--- LehmanHackathon2014/src/Testdb.java
+++ LehmanHackathon2014/src/Testdb.java
@@ -6,12 +6,17 @@ public class Testdb {
 
 	public static void main(String args[]){
 
-		ArrayList<String> tag = new ArrayList<String>();
-		tag.add("Health");
-		tag.add("Charity");
-		tag.add("Design");
-		System.out.println(tag.toString());
-		System.out.println(Database.tagSearch(tag).toString());
+		ArrayList<String> tags = new ArrayList<String>();
+		tags.add("Health");
+		tags.add("Charity");
+		tags.add("Design");
+		System.out.println(tags.toString());
+		System.out.println(Database.findEvents(tags).toString());
+//		System.out.println(Database.getLocation("Hackathon"));
+//		System.out.println(Database.getTime("Hackathon"));
+//		System.out.println(Database.getTags("Hackathon").toString());
+//		System.out.println(Database.getCost("Hackathon"));
+//		System.out.println(Database.getInfo("Hackathon"));
 	}
 	
 }
